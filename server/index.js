e3da796d0dc8667cfbf27d847ac83294829fb009
emanuel caro <darkemlord@gmail.com>
2021-12-14 01:09:39 +0900

    database connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log('sabor papis')
+const mongoose = require('mongoose');
+require('dotenv').config( { path: ".env" }); //calling the ENV
+
+const connectDataBase = async () => {
+  try{
+    await mongoose.connect(process.env.BBDD,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('Connection success')
+  } catch (error) {
+    console.log('Connection error')
+  }
+};
+
+connectDataBase();
