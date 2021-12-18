608f81cfb8a7b0916b178343efc5436493b65e15
emanuel caro <darkemlord@gmail.com>
2021-12-18 17:17:12 +0900

    refactoring code usercontroller folder created

---
+++ server/controllers/User.js
@@ -0,0 +1,31 @@
+const User = require('../models/user')
+const bcryptjs = require('bcryptjs');
+
+const register = async (input) => {
+  const newUser = input;
+  newUser.email = newUser.email.toLowerCase();
+  newUser.username = newUser.username.toLowerCase();
+  const {email, username, password } = newUser;
+
+  const foundEmail = await User.findOne({ email });
+  if(foundEmail) throw new Error('Email already in use');
+
+  const foundUsername = await User.findOne({username });
+  if(foundUsername) throw Error('Username already in use');
+  //encrypt password
+
+  const salt = await bcryptjs.genSaltSync(10);
+  newUser.password = await bcryptjs.hash(password, salt);
+  try{
+    const user = new User(newUser);
+    user.save();
+    console.log('user saved!')
+    return user;
+  }catch(err){
+    console.log(err)
+  }
+};
+
+module.exports = {
+  register,
+};
--- server/gql/resolvers.js
+++ server/gql/resolvers.js
@@ -1,5 +1,5 @@
-const User = require('../models/user')
-const bcryptjs = require('bcryptjs');
+const userController = require('../controllers/User');
+console.log(userController)
 
 const resolvers = {
   Query: {
@@ -9,31 +9,8 @@ const resolvers = {
     },
   },
   Mutation: {
-    register: async (_, { input }) => {
-      const newUser = input;
-      newUser.email = newUser.email.toLowerCase();
-      newUser.username = newUser.username.toLowerCase();
-      const {email, username, password } = newUser;
-
-      const foundEmail = await User.findOne({ email });
-      if(foundEmail) throw new Error('Email already in use');
-
-      const foundUsername = await User.findOne({username });
-      if(foundUsername) throw Error('Username already in use');
-      //encrypt password
-
-      const salt = await bcryptjs.genSaltSync(10);
-      newUser.password = await bcryptjs.hash(password, salt);
-      try{
-        const user = new User(newUser);
-        user.save();
-        console.log('user saved!')
-        return user;
-      }catch(err){
-        console.log(err)
-      }
-    }
-  }
+    register: async (_, { input }) => userController.register(input)
+  },
 }
 
 module.exports = resolvers
