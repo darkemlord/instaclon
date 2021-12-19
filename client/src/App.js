a3f000a6f5b63f95887757562ae0363a9930056f
emanuel caro <darkemlord@gmail.com>
2021-12-19 22:34:12 +0900

    sass installed reeady to use

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'semantic-ui-react';
 
 
+
 function App() {
   return (
     <div className="container">
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
