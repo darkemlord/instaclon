15269b4c8bcbb55940e6cbde88644fd2d48fc6ea
emanuel caro <darkemlord@gmail.com>
2021-12-20 22:31:53 +0900

    refactirong form attributes

--- client/src/components/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Auth/RegisterForm/RegisterForm.js
@@ -2,16 +2,21 @@ import React from 'react';
 import {Form, Button} from 'semantic-ui-react';
 import './RegisterForm.scss';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export const RegisterForm = (props) => {
   const { setShowLogin } = props;
 
   const formik = useFormik({
     initialValues: initialValues() ,
-    validationSchema: null,
+    validationSchema: Yup.object({
+      name: Yup.string().required(true),
+      username: Yup.string().required(true),
+    }),
     onSubmit: (formValue) => {
       console.log('Form sended!')
       console.log(formValue)
+      formik.handleReset();
     }
   })
 
@@ -19,11 +24,43 @@ export const RegisterForm = (props) => {
     <>
       <h2 className='register-form-title'>Sign in here</h2>
       <Form className='register-form' onSubmit={formik.handleSubmit}>
-        <Form.Input type="text" placeholder = "Name" name="name" onChange={formik.handleChange}/>
-        <Form.Input type="text" placeholder = "Username" name="username" onChange={formik.handleChange}/>
-        <Form.Input type="text" placeholder = "Email" name="email" onChange={formik.handleChange}/>
-        <Form.Input type="password" placeholder = "Password" name="password" autoComplete="on" onChange={formik.handleChange}/>
-        <Form.Input type="password" placeholder = "Confirm Password" name="confirmPassword" autoComplete="on" onChange={formik.handleChange}/>
+        <Form.Input
+          type="text"
+          placeholder = "Name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          />
+        <Form.Input
+          type="text"
+          placeholder = "Username"
+          name="username"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+        />
+        <Form.Input
+          type="text"
+          placeholder = "Email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <Form.Input
+          type="password"
+          placeholder = "Password"
+          name="password"
+          autoComplete="on"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        <Form.Input
+          type="password"
+          placeholder = "Confirm Password"
+          name="confirmPassword"
+          autoComplete="on"
+          onChange={formik.handleChange}
+          value={formik.values.confirmPassword}
+        />
         <Button className='btn-submit' type="submit">Sign In</Button>
       </Form>
     </>
