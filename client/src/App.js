0dad3d895d2a26f4753819e8fea5283196a2e31a
emanuel caro <darkemlord@gmail.com>
2021-12-20 10:44:08 +0900

    Authentication page created and connected with app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Button } from 'semantic-ui-react';
 import client from './config/apollo';
 import { ApolloProvider } from '@apollo/client'
+import Auth from './pages/Auth';
 
 
 function App() {
+  const [auth, setAuth ] = useState(undefined);
   return (
     < ApolloProvider client={client} >
-      <div className="container">
-        <h1>hello bootstrap</h1>
-        <Button primary >Hello World</Button>
-      </div>
+      {!auth ? <Auth /> : <h1>Estas logueado</h1>}
     </ApolloProvider>
   );
 }
---
+++ client/src/pages/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Auth.scss';
+
+export const Auth = () => {
+  return (
+    <div>
+      <h1>Estamos en Auth!</h1>
+    </div>
+  )
+}
+
+export default Auth;
