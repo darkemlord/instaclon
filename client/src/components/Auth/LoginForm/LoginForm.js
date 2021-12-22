7f942617d49b16df0b00bae2d9fafeda9e545f71
emanuel caro <darkemlord@gmail.com>
2021-12-22 14:47:39 +0900

    formik applied in the login form

--- client/src/components/Auth/LoginForm/LoginForm.js
+++ client/src/components/Auth/LoginForm/LoginForm.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Form, Button} from 'semantic-ui-react';
 import './LoginForm.scss';
+import { useFormik } from 'formik';
 
 const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: null,
+    onSubmit: (formData) => {
+      console.log(formData)
+    }
+  })
   return (
-    <Form className="login-form">
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
       <h2>log in to see what your friends are posting!</h2>
       <Form.Input
         type="text"
         placeholder="Email"
         name="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
       >
       </Form.Input>
 
       <Form.Input
-        type="text"
+        type="password"
         placeholder="Password"
         name="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        autoComplete="on"
       ></Form.Input>
 
       <Button type="submit" className='btn-submit'>
@@ -25,5 +38,10 @@ const LoginForm = () => {
     </Form>
   )
 }
-
+const initialValues = () => {
+  return {
+    email: '',
+    password: '',
+  }
+}
 export default LoginForm;
