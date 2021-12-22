a06c9af813e8c25faae68e49d7148af2e271efce
emanuel caro <darkemlord@gmail.com>
2021-12-22 12:10:41 +0900

    form Register and Login css styles updated

--- client/src/components/Auth/LoginForm/LoginForm.js
+++ client/src/components/Auth/LoginForm/LoginForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Form, Button} from 'semantic-ui-react'
+import { Form, Button} from 'semantic-ui-react';
+import './LoginForm.scss';
 
 const LoginForm = () => {
   return (
     <Form className="login-form">
+      <h2>log in to see what your friends are posting!</h2>
       <Form.Input
         type="text"
         placeholder="Email"
