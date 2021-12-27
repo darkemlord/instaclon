8afcbe70c099fc6723b43731eca2a47e42b97c84
emanuel caro <darkemlord@gmail.com>
2021-12-27 13:57:24 +0900

    pages folders created and new pages

---
+++ client/src/pages/Error404/Error404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Error.scss'
+
+export default function Error404() {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useAuth } from '../../hooks/useAuth';
+import './Home.scss';
 
 export default function Home() {
-  const auth = useAuth();
-  console.log(auth)
   return (
     <div>
       <h1>Estas logueado sabroso</h1>
