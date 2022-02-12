475cdbc86c90cc78a26559656714bc913309dfe2
emanuel caro <darkemlord@gmail.com>
2022-02-12 11:11:27 +0900

    navbar header content updated

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Container, Grid, Image } from 'semantic-ui-react';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <div className="header">
+      <Container>
+        <Grid>
+          <Grid.Column width={3} className='header__logo'>
+            <p>Logo</p>
+          </Grid.Column>
+
+          <Grid.Column width={10} className='header__logo'>
+            <p>Search</p>
+          </Grid.Column>
+
+          <Grid.Column width={3} className='header__logo'>
+            <p>Options</p>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
   )
 }
 
--- client/src/layauts/LayautBasic.js
+++ client/src/layauts/LayautBasic.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
-import Header from '../components/Header/Header';
+import Header from '../components/Header';
 
 export const LayautBasic = (props) => {
   return (
