b4a949322e600fa6404c6e22f7971879580768c8
emanuel caro <darkemlord@gmail.com>
2022-02-12 22:09:20 +0900

    avatarform component created

---
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AvatarForm.scss';
+
+const AvatarForm = () => {
+  return (
+    <div>
+      <h2>avatar form</h2>
+    </div>
+  )
+}
+
+export default AvatarForm;
