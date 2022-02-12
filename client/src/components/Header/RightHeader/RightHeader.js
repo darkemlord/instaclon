3e0c581bb7a2824d18ea06cc01e93e7bd3350786
emanuel caro <darkemlord@gmail.com>
2022-02-12 11:41:36 +0900

    right header style updated

--- client/src/components/Header/RightHeader/RightHeader.js
+++ client/src/components/Header/RightHeader/RightHeader.js
@@ -3,6 +3,7 @@ import './RightHeader.scss';
 import { Icon, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
+import Avatar from '../../../assets/images/avatar.png';
 
 
 const RightHeader = () => {
@@ -10,6 +11,10 @@ const RightHeader = () => {
     <div className='right-header'>
       <Link to='/'>
         <Icon name='home' />
+      </Link>
+      <Icon name='plus'/>
+      <Link>
+        <Image src={Avatar} avatar/>
       </Link>
     </div>
   )
