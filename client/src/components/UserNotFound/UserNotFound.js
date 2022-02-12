653d67d50d060ad02f56eeec4b182bbf284b32f1
emanuel caro <darkemlord@gmail.com>
2022-02-12 16:01:47 +0900

    user not found component finished

--- client/src/components/UserNotFound/UserNotFound.js
+++ client/src/components/UserNotFound/UserNotFound.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './UserNotFound.scss';
+import { Link } from 'react-router-dom'
 
 const UserNotFound = () => {
   return (
     <div className='user-not-found'>
       <p>Sorry This page isn't available</p>
       <p>The link you followed may be broken, or the page may have been removed.</p>
+      <Link to='/'>Come back to main page</Link>
     </div>
   )
 }
