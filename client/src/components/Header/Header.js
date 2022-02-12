20f5ba2bd881e174dfbee3e6e7a33d53ffeab014
emanuel caro <darkemlord@gmail.com>
2022-02-12 11:19:27 +0900

    react router dom imported from header to add new link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Grid, Image } from 'semantic-ui-react';
 import Logo from '../../assets/images/instaclone.png'
 import './Header.scss';
