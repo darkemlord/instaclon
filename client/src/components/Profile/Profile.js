fa516e56457d7e5af6e9c1b53912d643e7db5909
emanuel caro <darkemlord@gmail.com>
2022-02-12 15:12:21 +0900

    profile component updated

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../../gql/user'
+import './Profile.scss';
 
-const Profile = () => {
+const Profile = (props) => {
+  const { username } = props;
+  const { data, loading, error } = useQuery(GET_USER, {
+    variables: { username }
+  });
+
+  if(loading) return null;
+  if(error) return <h1>User doesn't exist</h1>
+
+  const { getUser } = data
+  console.log(getUser)
   return (
-    <div>Profile</div>
+    <div>
+      <h1>{username}</h1>
+    </div>
   )
 }
 
