815a6f4bcdb4b7c6a7295055e7133a2862dca0a8
emanuel caro <darkemlord@gmail.com>
2022-02-13 11:17:17 +0900

    createn aws upload system

---
+++ server/utils/aws-upload-image.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '.env'});
+
+const AWS = require('aws-sdk');
+const ID = process.env.AWS_ID
+const SECRET = process.env.AWS_SECRET
+const BUCKET = process.env.AWS_BUCKET
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
+});
+
+const awsUploadImage = async (file, filePath) => {
+  const params = {
+    Bucket: BUCKET,
+    key: `${filePath}`,
+    Body: file
+  }
+
+  try {
+    const response = await s3.upload(params).promise();
+    return response.Location;
+  } catch (err) {
+    console.log(err)
+    throw new Error()
+  }
+}
+
+module.exports = awsUploadImage;
