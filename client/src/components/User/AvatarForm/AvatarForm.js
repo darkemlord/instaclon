89dec3cf860f7457a03a9352e2a8b23a1d28c0c2
emanuel caro <darkemlord@gmail.com>
2022-02-13 09:37:10 +0900

    upload picture functionality in buttons created

--- client/src/components/User/AvatarForm/AvatarForm.js
+++ client/src/components/User/AvatarForm/AvatarForm.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './AvatarForm.scss';
 import { Button } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
 
 const AvatarForm = (props) => {
-
   const { setShowModal } = props
+
+  const onDrop = useCallback((acceptedFile) => {
+    console.log(acceptedFile)
+  }, [])
+
+  const { getRootProps, getInputProps} = useDropzone({
+    accept: 'image/jpeg, image/png',
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  }
+  );
   return (
     <div className='avatar-form'>
-      <Button>Upload your picture</Button>
+      <Button {...getRootProps()} >Upload your picture</Button>
       <Button>Delete current picture</Button>
       <Button onClick={() =>  setShowModal(false)}>Cancel</Button>
+      <input {...getInputProps()}/>
     </div>
   )
 }
