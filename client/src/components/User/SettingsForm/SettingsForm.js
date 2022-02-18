fca64ae62126dd61517fbacc13d05a629576cbf2
emanuel caro <darkemlord@gmail.com>
2022-02-18 16:18:12 +0900

    updating logout function, removing the cache on log out

--- client/src/components/User/SettingsForm/SettingsForm.js
+++ client/src/components/User/SettingsForm/SettingsForm.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import './SettingsForm.scss';
+import { useApolloClient } from '@apollo/client'
+import { useHistory } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth';
 
 const SettingsForm = (props) => {
   const {setShowModal} = props;
+  const history = useHistory();
+  const client = useApolloClient();
   const { logout } = useAuth();
+
+  const onLogout = () => {
+    client.clearStore();
+    logout();
+    history.push('/')
+  }
+
   return (
     <div className='settings-form'>
       < Button>Change password</Button>
       < Button>Change email</Button>
       < Button>Change description</Button>
       < Button>Change website</Button>
-      < Button onClick={logout}>Log Out</Button>
+      < Button onClick={onLogout}>Log Out</Button>
       <Button onClick={() => { setShowModal(false)}}>Cancel</Button>
     </div>
   )
